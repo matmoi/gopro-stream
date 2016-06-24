26fac67e795e4cdf8681d0d3aaf77af6fde48514
matthiem <matthiem@microsoft.com>
2016-06-25 01:24:54 +0200

    lib+examples

---
+++ examples/gopro2file.js
@@ -0,0 +1,19 @@
+﻿var fs = require('fs');
+var GoproStream = require('../lib/index.js');
+
+var outputStream = fs.createWriteStream("gopro.mp4");
+
+var goproStream = new GoproStream();
+goproStream.on('data', function (buffer) {
+    outputStream.write(new Buffer(buffer));
+});
+goproStream.startStream();
+
+console.log("Start camera (ctrl+c to stop)");
+
+process.on('SIGINT', () => {
+    console.log("exit");
+    outputStream.end();
+    goproStream.stopStream();
+    process.exit();
+});
---
+++ examples/livestream/livestream.js
@@ -0,0 +1,36 @@
+﻿require('log-timestamp');
+var GoproStream = require('../../lib/index.js');
+var express = require('express');
+var fs = require('fs');
+
+var HTTP_PORT = 8089;
+var WEBSOCKET_PORT = 8084;
+
+console.log("Start");
+
+var app = express();
+var goproStream = new GoproStream();
+var socketServer = new (require('ws').Server)({ port: WEBSOCKET_PORT });
+
+
+app.use('/index', express.static(__dirname + '/static'));
+app.listen(HTTP_PORT);
+
+goproStream.on('data', function (buffer) {
+    console.log(`Send ${buffer.byteLength} bytes to ${socketServer.clients.length} connected clients`);
+    socketServer.broadcast(buffer);
+});
+
+socketServer.broadcast = function broadcast(data) {
+    socketServer.clients.forEach(function each(client) {
+        if (client.readyState === client.OPEN) {
+            client.send(data);
+        }
+    });
+};
+socketServer.on('connection', function (socket) {
+    console.log('[websocket] New WebSocket Connection (' + socketServer.clients.length + ' total)');
+    socket.binaryType = 'arraybuffer';
+});
+
+goproStream.startStream();
